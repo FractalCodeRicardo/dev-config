deb994004d4f854e1a16538b89a7636812993fd8
Ricardo Serrato B. <rsbse@hotmail.com>
2024-05-19 19:44:42 -0600

    Plugins added

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 require("config.lazy")
 require("config.keymaps")
 
+require("mason").setup()
+require("mason-lspconfig").setup()
+local lspconfig = require "lspconfig"
+
+lspconfig.omnisharp_mono.setup {}
+
 require("tokyonight").setup({
 	style="storm"
 })

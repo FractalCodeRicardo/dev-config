2da5d5fbf613777fd8668bb67db5fc4003b8f4d7
Ricardo Serrato B. <rsbse@hotmail.com>
2024-05-25 17:20:15 -0600

    lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,5 +7,12 @@ return {
     config = function()
         require("neodev").setup()
         require("lspconfig").lua_ls.setup({})
+        local os = require('os')
+        local omnisharp_server_location = os.getenv('OMNISHARP_LANGUAGE_SERVER')
+
+        require("lspconfig").omnisharp.setup({
+           -- cmd = { "dotnet", omnisharp_server_location }
+            cmd = { "dotnet", "/home/ricardo/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" }
+        })
     end
 }

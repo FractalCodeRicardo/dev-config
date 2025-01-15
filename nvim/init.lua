ebfce3d4414b3e08433bd443b337bb159e343e3d
Ricardo Serrato B. <rsbse@hotmail.com>
2025-01-14 21:47:00 -0600

    changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
 require("config.lazy")
 require("config.keymaps")
 require("config.options")
---vim.cmd("colorscheme tokyonight-night")
+vim.cmd("colorscheme tokyonight-night")
 --vim.cmd("colorscheme catppuccin-mocha")
 --vim.cmd("colorscheme onedark")
-vim.cmd("colorscheme one_monokai")
+--vim.cmd("colorscheme one_monokai")
 
 

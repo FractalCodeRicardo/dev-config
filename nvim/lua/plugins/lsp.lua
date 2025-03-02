e5db8e819f6c879a0d8eae3e2cb500d7a1e71623
Ricardo Serrato B. <rsbse@hotmail.com>
2025-03-01 20:38:29 -0600

    :pencil: add: roslyn lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,82 +1,87 @@
 return {
-   {
-      "williamboman/mason.nvim",
-      lazy = true,
-      config = function()
-         require('mason').setup()
-      end
-   },
+    {
+        "williamboman/mason.nvim",
+        lazy = true,
+        config = function()
+            require('mason').setup({
+                registries = {
+                    'github:Crashdummyy/mason-registry',
+                    'github:mason-org/mason-registry',
+                },
+            })
+        end
+    },
 
-   {
-      "williamboman/mason-lspconfig.nvim",
-      lazy = true,
-      dependencies = {
-         "williamboman/mason.nvim"
-      },
-      opts = {
-         auto_install = true,
-      },
-      config = function()
-         require('mason-lspconfig').setup({
-            ensure_installed = { "lua_ls" }
-         })
-      end
-   },
-   {
-      "neovim/nvim-lspconfig",
-      event = { "BufReadPre", "BufNewFile" },
-      -- lazy = true,
-      dependencies = {
-         "williamboman/mason.nvim",
-         "williamboman/mason-lspconfig.nvim",
-      },
-      config = function()
-         local capabilities = require('cmp_nvim_lsp').default_capabilities()
-         local lspconfig = require('lspconfig')
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = true,
+        dependencies = {
+            "williamboman/mason.nvim"
+        },
+        opts = {
+            auto_install = true,
+        },
+        config = function()
+            require('mason-lspconfig').setup({
+                ensure_installed = { "lua_ls" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        -- lazy = true,
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require('lspconfig')
 
-         -- Lua
-         lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-               Lua = {
-                  runtime = {
-                     version = 'LuaJIT', -- Use LuaJIT for Neovim
-                  },
-                  diagnostics = {
-                     globals = { 'vim' }, -- Recognize `vim` as a global
-                  },
-                  workspace = {
-                     library = vim.api.nvim_get_runtime_file("", true), -- Make LSP aware of Neovim runtime
-                     checkThirdParty = false,                           -- Disable third-party library warnings
-                  },
-                  telemetry = {
-                     enable = false, -- Disable telemetry for privacy
-                  },
-               },
-            },
-         })
+            -- Lua
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = 'LuaJIT', -- Use LuaJIT for Neovim
+                        },
+                        diagnostics = {
+                            globals = { 'vim' }, -- Recognize `vim` as a global
+                        },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true), -- Make LSP aware of Neovim runtime
+                            checkThirdParty = false,                    -- Disable third-party library warnings
+                        },
+                        telemetry = {
+                            enable = false, -- Disable telemetry for privacy
+                        },
+                    },
+                },
+            })
 
-         --omnisharp
-         lspconfig.omnisharp.setup({
-            capabilities = capabilities,
-            enable_roslyn_analysers = true,
-            enable_import_completion = true,
-            organize_imports_on_format = true,
-            enable_decompilation_support = true,
-            filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets' }
-         })
+            --omnisharp
+          --  lspconfig.omnisharp.setup({
+          --      capabilities = capabilities,
+          --      enable_roslyn_analysers = true,
+          --      enable_import_completion = true,
+          --      organize_imports_on_format = true,
+          --      enable_decompilation_support = true,
+          --      filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets' }
+          --  })
 
 
-         --typscript
-         -- Configure tsserver for React and JavaScript
-         lspconfig.ts_ls.setup({
-            capabilities = capabilities,
-         })
+            --typscript
+            -- Configure tsserver for React and JavaScript
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities,
+            })
 
-         lspconfig.jsonls.setup{}
+            lspconfig.jsonls.setup {}
 
-        -- lspconfig.powershell_es.setup {}
-      end
-   }
+            -- lspconfig.powershell_es.setup {}
+        end
+    }
 
 }

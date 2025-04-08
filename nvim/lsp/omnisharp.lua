3883b889b2e61d462c6689d71f8cc84ab4850c29
Ricardo Serrato B. <rsbse@hotmail.com>
2025-04-07 20:56:00 -0600

    :arrow_up: add: here we go again

--- nvim/lsp/omnisharp.lua
+++ nvim/lsp/omnisharp.lua
@@ -1,11 +1,12 @@
--- local capabilities = require("cmp_nvim_lsp").default_capabilities()
--- return {
---     cmd = {
---         "omnisharp",
---         "--languageserver",
---         "--hostPID", tostring(vim.fn.getpid())
---     },
---     capabilities = capabilities,
---     filetypes = { 'cs', 'vb' },
---     root_dir = vim.fn.getcwd()
--- }
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+return {
+    cmd = {
+        "omnisharp",
+        "--languageserver",
+        "--hostPID", tostring(vim.fn.getpid())
+    },
+     capabilities = capabilities,
+    filetypes = { 'cs', 'vb' },
+    root_markers = {".sln"}
+}
